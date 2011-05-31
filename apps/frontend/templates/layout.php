fce9421549d249e9a2383148d5ac6e75997f7104
SylwesterG <sylwester.golda@gazeta.pl>
2011-05-31 18:47:28 +0200

    zmiana

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -1,14 +1,56 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<!-- apps/frontend/templates/layout.php -->
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
   <head>
-    <?php include_http_metas() ?>
-    <?php include_metas() ?>
-    <?php include_title() ?>
+    <title>Finansowa</title>
     <link rel="shortcut icon" href="/favicon.ico" />
-    <?php include_stylesheets() ?>
     <?php include_javascripts() ?>
+    <?php include_stylesheets() ?>
   </head>
   <body>
-    <?php echo $sf_content ?>
+    <div id="container">
+     <div id="header">
+        <div class="content">
+         <h1><a href="<?php echo url_for('view/index') ?>">
+            <img src="/images/logo.jpg" alt="Jobeet Job Board" />
+          </a></h1>
+
+        </div>
+      </div>
+ 
+      <div id="content">
+        <?php if ($sf_user->hasFlash('notice')): ?>
+          <div class="flash_notice">
+            <?php echo $sf_user->getFlash('notice') ?>
+          </div>
+        <?php endif ?>
+ 
+        <?php if ($sf_user->hasFlash('error')): ?>
+          <div class="flash_error">
+            <?php echo $sf_user->getFlash('error') ?>
+          </div>
+        <?php endif ?>
+ 
+        <div class="content">
+          <?php echo $sf_content ?>
+        </div>
+      </div>
+ 
+      <div id="footer">
+        <div class="content">
+          <span class="symfony">
+            <img src="/images/jobeet-mini.png" />
+            powered by <a href="http://www.symfony-project.org/">
+            <img src="/images/symfony.gif" alt="symfony framework" />
+            </a>
+          </span>
+          <ul>
+            <li><a href="">O Informatyce finansowej</a></li>
+            <li class="last"><a href="">Affiliates</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </body>
 </html>
